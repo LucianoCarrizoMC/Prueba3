d57be46af48fd4ba79a3b960d99726994c7e3730
LucianoCarrizoMC <lucianocarrizo022@gmail.com>
2024-06-05 10:10:02 -0400

    Fin parte1
    1. no terminado
    2.a listo
    2.b listo
    2.c listo
    2.d listo
    2.e por terminar
    2.g no iniciado

---
+++ assests/js/credenciales.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyClcPg40falOgqiZidyqqUaG6TuCgehQyw",
+    authDomain: "prueba-proyect-96dc0.firebaseapp.com",
+    projectId: "prueba-proyect-96dc0",
+    storageBucket: "prueba-proyect-96dc0.appspot.com",
+    messagingSenderId: "864775598751",
+    appId: "1:864775598751:web:5b124b04d42b75466528aa"
+  };
+
+  //crendenciales :V
---
+++ assests/js/scrip.js
@@ -0,0 +1,38 @@
+
+
+//window.addEventListener("load",()=>{
+    // document.getElementById("btncambiocolor").addEventListener("click",cambiarcolor)
+    // document.getElementById("btnregistrar").addEventListener("click",registrar)
+   // })
+
+
+
+
+function registrar(){
+    let Ein1=document.getElementById("input1");
+    let Ein2=document.getElementById("input2")
+    let Ein3=document.getElementById("input3")
+    let Ein4=document.getElementById("input4")
+    let Ein5=document.getElementById("input5")
+    let Ein6=document.getElementById("input6")
+    let Ein7=document.getElementById("input7")
+    let Ein8=document.getElementById("input8")
+
+    let Vin1=Ein1.value
+    let Vin2=Ein2.value
+    let Vin3=Ein3.value
+    let Vin4=Ein4.value
+    let Vin5=Ein5.value
+    let Vin6=Ein6.value
+    let Vin7=Ein7.value
+    let Vin8=Ein8.value
+
+
+
+
+}
+
+function cambiarcolor(){
+    console.log("copetee")
+    
+}
